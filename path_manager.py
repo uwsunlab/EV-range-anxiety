8cc92d1629caf144b929e629b095dd5211179c2d
Hemanth N R <hemanthnr.12@gmail.com>
2025-01-03 01:01:49 -0800

    updated paths

---
+++ path_manager.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+#dir paths
+ROOT_DIR = Path(__file__).parent
+DATASET_DIR = ROOT_DIR / 'datasets'
+MISC_DIR = ROOT_DIR / 'misc'
+MODELS_DIR = ROOT_DIR / 'ML_and_simulink_models'
+
+#specific folder paths
+BATT_DATA_DIR = DATASET_DIR / 'battery_dataset'
+VEL_DATA_DIR = DATASET_DIR / 'velocity_dataset'
+CURRENT_MODEL_DIR = MODELS_DIR /'current_based_ML_models'
+FORECAST_MODEL_RESULT_DIR = CURRENT_MODEL_DIR / 'forecast_model_results'
+
+##specific notebook file paths
+FORECAST_DATA_NB = MISC_DIR / 'forecast_data_create.ipynb'
+REF_NB = MISC_DIR / 'Cons_brk_tests.ipynb'
+TRAIN_DATA_NB = MISC_DIR / 'Database_create.ipynb'
+TEST_DATA_NB = MISC_DIR / 'Test_data_create.ipynb'
